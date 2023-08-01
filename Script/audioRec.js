13705b493c22f127dfe72a0b319a4fe77092b1a6
Justin <JustinChen3946@gmail.com>
2023-07-31 21:15:00 -0400

    Audio recording start

--- Script/audioRec.js
+++ Script/audioRec.js
@@ -3,33 +3,82 @@
     General Gist: Media Recorder takes a MediaDevice API to know where it's getting audio from
 */
 
-const audioRecorder = {
+const AudioRecorder = function() {
+
+    /**
+     * Stores whether or not audio recorder is active
+     */
+    this.recording = false;
+
+    /**
+     * Stores audio data. This should be an array of blobs
+     */
+    this.audioData = [];
+
+    /**
+     * The actual thing doing the data. Make sure you call this.start() so the `mediaRecorder` exists to begin with.
+     */
+    this.mediaRecorder = null;
 
     /**
      * Starts recording audio
      * @returns {Promise} Resolves if audio recording has successfully started
      */
-    start: () => new Promise((resolve, reject) => {
+    this.start = function() 
+    {
+        return new Promise((resolve, reject) => {
 
-        //Check if we have Audio API is even supported
-        if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia)
-        {
-            reject(new OverconstrainedError("No Audio API detected"));
-            return; //Don't think we need a return here
-        }
-    }),
+            //Check if we have Audio API is even supported
+            if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia)
+            {
+                //If mediaRecorder is on, now it doesn't do anything because microphone perms are revoked
+                this.mediaRecorder = null;
+
+                reject(new OverconstrainedError("No Audio API detected"));
+                return; //Don't think we need a return here
+            }
+
+            navigator.mediaDevices.getUserMedia({audio: true, video: false})
+                .then(stream => {
+                    this.recording = true;
+                    
+                    //Every set interval, we want new data to be processed in the FFT
+                    this.audioData = [];
+
+                    if (!this.mediaRecorder)
+                    {
+                        this.mediaRecorder = new MediaRecorder(stream);
+
+                        this.mediaRecorder.ondataavailable(e => {
+                            this.audioData.push(e.data);
+                        });
+                    }
+
+                    //Start recording!
+                    this.mediaRecorder.start();
+                });
+            });
+    }
+
     /**
      * Stops recording audio
      * @returns {Promise} Resolves an audio blob
      */
-    stop: () => new Promise((resolve, reject) => {
+    this.stop = function() {
+        return new Promise((resolve, reject) => {
 
-    }),
+        })
+    };
+
     /**
      * Just completely cancels process of creating audio
-     * @returns 
+     * @returns {Promise}
      */
-    cancel: () => new Promise((resolve, reject) => {
+    this.cancel = function() {
+        return new Promise((resolve, reject) => {
 
-    })
+        });
+    }
 };
+
+export default AudioRecorder;
